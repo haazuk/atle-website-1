5d7b5e62ae7a1739ab2cad5e8995a40c9f368d5a
HazikChaudhry <3692758-HazikChaudhry@users.noreply.replit.com>
2023-08-07 06:05:25 +0000

    changes

--- script.js
+++ script.js
@@ -1,3 +1,13 @@
+// Loading Screen
+window.addEventListener("load", function(){
+	let hoverImagesContainer = document.getElementById('imageHover');
+	let loadingScreen = document.getElementById('loadingScreen');
+	loadingScreen.style.opacity = '0';
+	loadingScreen.style.zIndex = '-1';
+	hoverImagesContainer.style.opacity = '1';
+});
+
+
 // sorry for the spaghetti code and redundant variables, i wasn't exactly a good coder back then
 
 const cols = 3;

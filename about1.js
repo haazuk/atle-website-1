89cd132767de80e10b18072eafce18cd2f78f0e6
haazuk <99319319+haazuk@users.noreply.github.com>
2023-08-12 01:01:50 +0000

    did some cahnges

--- about1.js
+++ about1.js
@@ -3,7 +3,7 @@ window.addEventListener("load", function(){
 	let hoverImagesContainer = document.getElementById('imageHover');
 	let loadingScreen = document.getElementById('loadingScreen');
 	loadingScreen.style.opacity = '0';
-	loadingScreen.style.zIndex = '-1';
+	loadingScreen.style.zIndex = '-2';
 	hoverImagesContainer.style.opacity = '1';
 });
 

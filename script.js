654103af1e0a092b119e817e7e82a58af72c6c62
haazuk <99319319+haazuk@users.noreply.github.com>
2023-08-20 23:46:54 +0000

    The addition of a OAuth API system that redirects to pages in the website, uses itch.io system

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 // Loading Screen
-window.addEventListener("load", function(){
-	let hoverImagesContainer = document.getElementById('imageHover');
-	let loadingScreen = document.getElementById('loadingScreen');
-	loadingScreen.style.opacity = '0';
-	loadingScreen.style.zIndex = '-1';
-	hoverImagesContainer.style.opacity = '1';
+window.addEventListener("load", function() {
+  let hoverImagesContainer = document.getElementById('imageHover');
+  let loadingScreen = document.getElementById('loadingScreen');
+  loadingScreen.style.opacity = '0';
+  loadingScreen.style.zIndex = '-1';
+  hoverImagesContainer.style.opacity = '1';
 });
 
 
@@ -191,8 +191,8 @@ window.addEventListener('wheel', wheel, false);
 
 //Navigation
 
-$(document).ready(function () {
-  $("html").on("DOMMouseScroll mousewheel", function (e) {
+$(document).ready(function() {
+  $("html").on("DOMMouseScroll mousewheel", function(e) {
     if (e.originalEvent.detail > 0 || e.originalEvent.wheelDelta < 0) {
       //alternative options for wheelData: wheelDeltaX & wheelDeltaY
       //scroll down
@@ -213,7 +213,7 @@ $(document).ready(function () {
   var $menu = $("#menu"),
     $menulink = $(".menu-link");
 
-  $menulink.click(function () {
+  $menulink.click(function() {
     $menulink.toggleClass("active");
     $menu.toggleClass("active");
     return false;
@@ -221,7 +221,7 @@ $(document).ready(function () {
 
   var toggled = 0;
 
-  $(".menu-link").click(function () {
+  $(".menu-link").click(function() {
     if (toggled == 0) {
       $(".bar3").stop().transition({ rotate: "45", "margin-top": "13px" });
       $(".bar2").stop().transition({ opacity: "0" }, "fast");
@@ -237,3 +237,4 @@ $(document).ready(function () {
     }
   });
 });
+

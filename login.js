dff111e7c9b1bd37a90ff6b91844374b2ad39bc6
haazuk <99319319+haazuk@users.noreply.github.com>
2023-11-09 09:24:34 +0000

    Initial login page design and test

---
+++ login.js
@@ -0,0 +1,8 @@
+
+function submitEmail() {
+    var emailInput = document.getElementById("emailInput").value;
+
+    // You can add validation or further processing here
+
+    alert("Email submitted: " + emailInput);
+}
